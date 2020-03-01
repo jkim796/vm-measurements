c0a6e8f38f4bc8869f5ae1df70a002fe4defd219
Juno Kim <juno@eng.ucsd.edu>
2020-03-01 14:39:05 -0800

    minor fix

--- experiments/run.py
+++ experiments/run.py
@@ -18,9 +18,22 @@ class Platform(Enum):
 
 
 class Benchmark(object):
+    RESULTS_CSV_DIR = './results'
 
     def __init__(self, platform=Platform.NATIVE):
         self.platform = platform
+
+    def run(self):
+        pass
+
+    def run_native(self):
+        pass
+
+    def run_docker(self):
+        pass
+
+    def parse_output(self, output):
+        pass
 
 
 class SysbenchCPU(Benchmark):
@@ -33,6 +46,7 @@ class SysbenchCPU(Benchmark):
         self.max_time = max_time
         self.cpu_max_prime = cpu_max_prime
         self.csv_headers = ['num_threads', 'max_time', 'cpu_max_prime', 'events_per_second']
+        self.csv_filename = 'sysbench_cpu.csv'
 
     def run(self):
         if self.platform == Platform.NATIVE:
@@ -61,20 +75,24 @@ class SysbenchCPU(Benchmark):
                 events_per_second = float(matches[0])
                 return events_per_second
 
-    def write_to_csv(self, data, filepath):
+    def write_to_csv(self, data):
         """Appends the given parameters and performance metric values to the csv file."""
         csv_row = {'num_threads': self.num_threads,
                    'max_time': self.max_time,
                    'cpu_max_prime': self.cpu_max_prime,
                    'events_per_second': data}
-        if not os.path.isdir('./results'):
-            os.mkdir('./results')
-        filepath = './results/' + filepath
+        # Check if results directory have been created
+        if not os.path.isdir(self.RESULTS_CSV_DIR):
+            os.mkdir(self.RESULTS_CSV_DIR)
+        filepath = os.path.join(self.RESULTS_CSV_DIR, self.csv_filename)
+
+        # Write CSV headers when creating file for first time
         if not os.path.isfile(filepath):
             with open(filepath, 'w') as csv_file:
                 csv_writer = csv.DictWriter(csv_file, fieldnames=self.csv_headers)
                 csv_writer.writeheader()
 
+        # Append row to csv file
         with open(filepath, 'a') as csv_file:
             csv_writer = csv.DictWriter(csv_file, fieldnames=self.csv_headers)
             csv_writer.writerow(csv_row)
@@ -122,7 +140,7 @@ class Dispatcher(object):
         sysbench_cpu = SysbenchCPU(num_threads, max_time, cpu_max_prime)
         output = sysbench_cpu.run()
         data = sysbench_cpu.parse_output(output)
-        sysbench_cpu.write_to_csv(data, 'sysbench_cpu.csv')
+        sysbench_cpu.write_to_csv(data)
 
     def dispatch_sysbench_memory(self, bench_config):
         pass
