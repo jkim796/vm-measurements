dd712ac8035e1bb25b455cdb8412c1193e405b9a
Juno Kim <juno@eng.ucsd.edu>
2020-03-02 10:58:03 -0800

    Docker works for sysbench

--- experiments/sysbench_cpu.py
+++ experiments/sysbench_cpu.py
@@ -4,7 +4,7 @@ import re
 import shlex
 import subprocess
 
-from experiments import WORKLOADS_DIR
+from experiments import WORKLOADS_DIR, BENCHMARK_TOOLS_DIR
 
 from benchmark import Benchmark, Platform
 
@@ -13,9 +13,12 @@ class SysbenchCPU(Benchmark):
     # Benchmark name as used in INI file sections
     BENCH_NAME = 'sysbench_cpu'
 
-    # Performance metric
+    # Performance metric for native output
     EVENTS_PER_SECOND = 'events per second'
     REGEX_FLOAT = '\d+\.\d+'
+
+    # Performance metric for Docker output
+    EVENTS_PER_SECOND_DOCKER = 'cpu_events_per_second'
 
     # Experiment parameters
     PARAM_NUM_THREADS = 'num_threads'
@@ -26,7 +29,7 @@ class SysbenchCPU(Benchmark):
     # Default Dockerfile provided by benchmark-tools (backed up to .Dockerfile)
     DEFAULT_DOCKERFILE_NAME = os.path.join(ORIGINAL_DOCKERFILE_DIR, '.Dockerfile')
     # Dockerfile we'll use with the parameters in INI file
-    DOCKERFILE_NAME = 'Dockerfile'
+    DOCKERFILE_NAME = os.path.join(ORIGINAL_DOCKERFILE_DIR, 'Dockerfile')
 
     def __init__(self, num_threads, max_time, cpu_max_prime, platform=Platform.NATIVE):
         super(SysbenchCPU, self).__init__(platform)
@@ -60,28 +63,59 @@ class SysbenchCPU(Benchmark):
         print(lines)
         print(len(lines))
 
+        # Sysbench-specific: find the last line
+        last_line = len(lines) - 1
+
         # Remove these lines - assumes ENV lines are grouped together
         del lines[env_linenos[0] : env_linenos[-1] + 1]
-        print(lines)
-        print(len(lines))
 
         # Create ENV declaration strings
-        env_decls = [f'ENV threads {self.num_threads}']
+        env_decls = [f'ENV threads {self.num_threads}\n',
+                     f'ENV max_time {self.max_time}\n',
+                     f'ENV cpu_max_prime {self.cpu_max_prime}\n']
 
         # Insert the new ENV decalrations to lines
+        lines[env_linenos[0] : env_linenos[0]] = env_decls
+
+        # Handle sysbench-specific Dockerfile issues: replace ${options} with other parameters
+        # This will be the last line in the Dockerflie
+        cmd = 'sysbench --threads=${threads} --max-time=${max_time} --cpu-max-prime=${cpu_max_prime} cpu run"]"'
+        lines[last_line] = cmd
 
         # Write to Dockerfile
+        new_dockerfile = self.DOCKERFILE_NAME
+        with open(new_dockerfile, 'w') as f:
+            for line in lines:
+                f.write(line)
 
         # Run perf.py script
-
+        perf_cmd = f'python3 {BENCHMARK_TOOLS_DIR}/perf.py run --env {BENCHMARK_TOOLS_DIR}/examples/localhost.yaml sysbench.cpu'
+        process = subprocess.run(shlex.split(perf_cmd),
+                                 stdout=subprocess.PIPE,
+                                 universal_newlines=True)
+        output_str = process.stdout
+        return output_str.split('\n')
 
     def parse_output(self, output):
+        if self.platform == Platform.NATIVE:
+            return self._parse_native_output(output)
+        elif self.platform == Platform.DOCKER:
+            return self._parse_docker_output(output)
+
+    def _parse_native_output(self, output):
         for line in output:
             if self.EVENTS_PER_SECOND in line:
                 print(f"[sysbench cpu] {line}")
                 matches = re.findall(self.REGEX_FLOAT, line)
                 # There should only be one floating point number
                 events_per_second = float(matches[0])
+                return events_per_second
+
+    def _parse_docker_output(self, output):
+        for line in output:
+            if self.EVENTS_PER_SECOND_DOCKER in line:
+                print(f'[sysbench cpu] {line}')
+                events_per_second = float(line.split(',')[1])
                 return events_per_second
 
     def write_to_csv(self, data):
